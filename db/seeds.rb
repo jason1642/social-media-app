67ce8b6f449dab47a01d5892d194ceb07f256e98
jasonc1642 <jason1642@gmail.com>
2020-05-17 10:27:00 -0400

    seed edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,7 @@ User.create(username: "jason1642", email: "smallfrmjrhefgy19112@email.com", pass
 User.create(username: "jason11231231642", email: "smallfry19muynhrbgv112@email.com", password: "edp123123445")
 User.create(username: "jasoasczxn1642", email: "smallfry19112@email.com", password: "edp41232rqwas45")
 User.create(username: "jason16umjthnbgvr42", email: "smallfrytbvrce19112@email.com", password: "edp4,mjynrhbgef45")
-# User.create(username: "jason16qdqqwwqd42", email: "smallfry19112qwdqwdqwdq@email.com", password: "edp4qwdqw45")
-# User.create(username: "jasqwfqwdszvon1642", email: "smallfry19zXV112@email.com", password: "zxVedp445")
+
 
 
 
