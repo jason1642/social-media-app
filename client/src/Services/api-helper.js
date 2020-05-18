81e7e99613e2b43d2dc791f7531bb82abc9ae465
jasonc1642 <jason1642@gmail.com>
2020-05-18 02:21:43 -0400

    message here

--- client/src/Services/api-helper.js
+++ client/src/Services/api-helper.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const baseUrl = process.env.NODE_ENV !== 'production' ? /* link to your heroku app. Example:*/'https://social-media-app-api.herokuapp.com/' : 'http://localhost:3000'
+// const baseUrl = 'http://localhost:3000'
 
 const api = axios.create({
   baseURL: baseUrl
