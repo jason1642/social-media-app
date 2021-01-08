0c2b9fb093fd48a38f7a5312c21f2280a464e091
Jason Cruz <jason1642@gmail.com>
2021-01-08 16:35:10 -0500

    styling changes

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -4,18 +4,19 @@ import './HomePage.css'
 import styled from 'styled-components'
 import SideBar from './SideBar.jsx'
 import PostFeedSection from './PostFeedSection.jsx'
-
+import CreatePostBox from './CreatePostBox.jsx'
 
 const HomePage = props => {
 
   const Main = styled.div`
-      width: 70%;
-      margin: 0 auto;
+    width: 70%;
+    margin: 0 auto;
     `
 
   const Container = styled.div`
     display : flex;
     margin: 0 auto;
+    margin-top: 16px;
     flex-direction: row;
     width: 90%;
   `;
@@ -24,12 +25,7 @@ const HomePage = props => {
 
   return (
     <Main>
-      <h3 className='latest-posts-title'>Latest Posts</h3>
-      <Link>
-        <div>
-          <input placeholder='Create a post' />
-        </div>
-      </Link>
+
       {/* Container to separate feed and sidebar */}
       <Container >
 
--- client/src/components/HomePage/SideBar.jsx
+++ client/src/components/HomePage/SideBar.jsx
@@ -7,15 +7,20 @@ const SideBar = () => {
     border: 1px solid black;
     width: 30%;
     height: 100%;
+    border-radius: 5px;
   `;
   const Title = styled.p`
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     font-weight: bold;
     font-size: 1.4em;
+    width: 100%;
+    border-bottom: 1px solid black;
+    padding-bottom: 10px;
   `;
   return (
     <Main>
       <Title>About PostTree</Title>
+      <p>Due to lack of moderation, there is no image sharing on this site.</p>
     </Main>
 
   )
