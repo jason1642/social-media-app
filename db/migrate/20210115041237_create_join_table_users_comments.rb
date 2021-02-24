d5b0dcef9727f821de838700a9294e1aafc08909
Jason Cruz <jason1642@gmail.com>
2021-02-23 19:43:37 -0500

    x

--- db/migrate/20210115041237_create_join_table_users_comments.rb
+++ db/migrate/20210115041237_create_join_table_users_comments.rb
@@ -2,7 +2,7 @@ class CreateJoinTableUsersComments < ActiveRecord::Migration[6.0]
   def change
     create_join_table :users, :comments do |t|
       t.index [:user_id, :comment_id]
-      t.index [:comment_id, :user_id]
+      # t.index [:comment_id, :user_id]
 
     end
   end
